c24fd8b6d8ec6c17985e646c7a7a48cf864b79d3
sharkdp <davidpeter@web.de>
2020-01-12 16:25:38 +0100

    Update interactive program

--- tests/interactive.cpp
+++ tests/interactive.cpp
@@ -5,25 +5,28 @@
 
 template <typename T>
 void showParameter(const painless::Parameter<T>& parameter) {
-  std::cout << parameter.name() << " = "
-            << painless::printer::to_string(*parameter) << "\n";
+  std::cout << "\x1b[33m" << parameter.name() << "\x1b[0m"
+            << " = "
+            << "\x1b[32m" << painless::printer::to_string(*parameter)
+            << "\x1b[0m\n";
 }
 
 int main() {
-  PAINLESS_PARAMETER(demo_float, 1.4f);
-  PAINLESS_PARAMETER(demo_int, 42);
   PAINLESS_PARAMETER(demo_bool, false);
+  PAINLESS_PARAMETER(demo_int, 42);
+  PAINLESS_PARAMETER(demo_float, 1.4f);
   PAINLESS_PARAMETER(demo_string, "hello world");
 
   int iteration = 0;
   while (true) {
     usleep(100000);
 
-    std::cout << "================ " << std::setw(6) << iteration
-              << " ================\n";
-    showParameter(demo_float);
-    showParameter(demo_int);
+    std::cout << "\x1b[2J\x1b[0;0H";
+    std::cout << "\x1b[1mIteration\x1b[0m " << std::setw(6) << iteration
+              << "\n\n";
     showParameter(demo_bool);
+    showParameter(demo_int);
+    showParameter(demo_float);
     showParameter(demo_string);
     std::cout << "\n";
 
