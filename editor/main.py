e466b79a43b9daa0f7902d12b032944bf5ca3520
sharkdp <davidpeter@web.de>
2020-01-28 21:52:31 +0100

    Initial draft for web-based editor

---
+++ editor/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os.path as path
+from os import listdir
+from os.path import isfile
+
+from flask import Flask, render_template, request
+
+
+BASE_PATH = "/tmp/painless"
+
+app = Flask(__name__)
+
+
+def parse_parameter_file(entry):
+    with open(path.join(BASE_PATH, entry), "r") as f:
+        value = f.readline()
+
+    return {"name": entry, "value": value}
+
+
+def get_parameters():
+    parameters = []
+    for entry in listdir(BASE_PATH):
+        if isfile(path.join(BASE_PATH, entry)):
+            parameters.append(parse_parameter_file(entry))
+    return parameters
+
+
+@app.route("/")
+def index():
+    parameters = get_parameters()
+    return render_template("base.html", parameters=parameters)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    data = request.json
+    name = data["name"]
+    value = data["value"]
+    print(f"Updating parameter '{name}' to value '{value}'")
+    with open(path.join(BASE_PATH, name), "w") as f:
+        f.write(value)
+    return ""
